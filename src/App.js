047fa659c4f7b10dc6b79537dbf82e11315b1e77
IshanSandhu <ishan.sandhu@mail.mcgill.ca>
2022-07-09 19:16:36 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className='main'>
+      <Container className="">
+      <Row className="m-auto align-self-center">
+        <Col xs={9}>
+          <iframe className="map"
+        src="https://ishan.users.earthengine.app/view/no2"
+        title="Earth Engine App"
+        width="100%"
+        height={600}
+        allowfullscreen
+
+        ></iframe>
+        </Col>
+        <Col>
+          <div className='info'>
+          <Card class="shadow p-3 mb-5 bg-white rounded" border='none'>
+            <Card.Body>
+              <Card.Title>NO2 Pollution</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">Please wait 20-30s for map to load</Card.Subtitle>
+              <Card.Text>
+                Explore this map displaying NO2 pollution before and during the pandemic.
+              </Card.Text>
+              <br></br>
+              <Card.Link href="https://code.earthengine.google.com/a01831b73728c90e81ae816802ad8419">Earth Engine Code</Card.Link>
+              <br></br>
+              <Card.Text>View the javascript used to render the data inside the interactive split map.
+              </Card.Text>
+
+
+              <Card.Link href="https://ishansandhu.ca/#portfolio">Made by Ishan Sandhu 👨‍💻</Card.Link>
+            </Card.Body>
+          </Card>
+          </div>
+        </Col>
+      </Row>
+      </Container>
+      </div>
   );
 }
 
